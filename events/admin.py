72eff7106f44963e0c1838ee894be4e09b5c8bdb
kuhanimkuu <davidmurunga@gmail.com>
2026-01-07 18:19:11 +0300

    Allowed debug to check logs for issues causing crash

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import User, Venue, Category, Event, Ticket, Booking, Payment
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Venue)
+admin.site.register(Category)
+admin.site.register(Event)
+admin.site.register(Ticket)
+admin.site.register(Booking)
+admin.site.register(Payment)
